2f400ad5226630eb2aabd06355a1328c5447a197
QuanNguyenHa <31998957+QuanNguyenHa@users.noreply.github.com>
2018-02-03 01:36:26 +0800

    Create Solution.java

---
+++ 2 - Operators/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double mealCost = in.nextDouble();
+        int tipPercent = in.nextInt();
+        int taxPercent = in.nextInt();
+        in.close();
+        
+        // Write your calculation code here.
+        double tip = mealCost * tipPercent / 100;
+        double tax = mealCost * taxPercent / 100;
+
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = (int) Math.round(mealCost + tip + tax);
+
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
